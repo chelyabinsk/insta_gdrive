04b334fbae4eb885aca1fb76d3f3026267341d48
Hobbledehoy <15384813+chelyabinsk@users.noreply.github.com>
2020-05-15 20:12:21 +0100

    Update clock.py

--- clock.py
+++ clock.py
@@ -3,8 +3,10 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 sched = BlockingScheduler()
 import insta_test
 
+insta_test.main()
+
 @sched.scheduled_job('cron', hour=6)
 def scheduled_job():
-    print('This job is run every weekday at 5pm.')
+    insta_test.main()
 
 sched.start()
